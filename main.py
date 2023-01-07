995579f049c7de68c0c176cb46136d268b40fffc
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-01-08 00:16:31 +0500

    modified

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
 import tkinter as tk
+from tkinter import filedialog
 
 # Create the main window
 window = tk.Tk()
 window.title("Text File Creator")
 
-
 # Create a function to create the text file
 def create_text_file():
-    # Get the name of the file from the entry field
-    file_name = file_name_entry.get()
+    # Open a file dialog to choose the save location
+    file_path = filedialog.asksaveasfilename()
 
     # Open the file in write mode
-    with open(file_name + ".txt", "w") as f:
+    with open(file_path, "w") as f:
         # Write the text to the file
         f.write(text_entry.get("1.0", "end-1c"))
-
-
-# Create a label for the file name field
-file_name_label = tk.Label(text="Enter the name of the file:")
-file_name_label.pack()
-
-# Create an entry field for the file name
-file_name_entry = tk.Entry()
-file_name_entry.pack()
 
 # Create a label for the text field
 text_label = tk.Label(text="Enter the text to be written to the file:")
